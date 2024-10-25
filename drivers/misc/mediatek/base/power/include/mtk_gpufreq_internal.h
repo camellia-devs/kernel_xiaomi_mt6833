4c806f17e51984845569395057b3f7cfbad83b67
claxten10 <claxten10@gmail.com>
2024-10-24 20:04:37 -0400

    drivers/misc/mediatek/base/power/include: import OEM changes
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- drivers/misc/mediatek/base/power/include/mtk_gpufreq_internal.h
+++ drivers/misc/mediatek/base/power/include/mtk_gpufreq_internal.h
@@ -18,9 +18,7 @@
 	|| defined(CONFIG_MACH_MT6873) \
 	|| defined(CONFIG_MACH_MT6853) \
 	|| defined(CONFIG_MACH_MT6893) \
-	|| defined(CONFIG_MACH_MT6833) \
-	|| defined(CONFIG_MACH_MT6877) \
-	|| defined(CONFIG_MACH_MT6781)
+	|| defined(CONFIG_MACH_MT6833)
 
 #include "gpufreq_v1/mtk_gpufreq_internal.h"
 
